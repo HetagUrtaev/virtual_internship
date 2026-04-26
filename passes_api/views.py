8af207b1e9767000eb0e3654ca8dfef872f1340a
Hetag <khetag.urtayev@mail.ru>
2026-04-26 23:14:41 +0300

    Добавить модель и структуру для сериализаторов

--- passes_api/views.py
+++ passes_api/views.py
@@ -1,1 +1,10 @@
+from rest_framework import viewsets
+from .serializers import PerevalSerializer
+from .models import Pereval
+
+
+class PerevalApiView(viewsets.ModelViewSet):
+    queryset =Pereval.objects.all()
+    serializer_class = PerevalSerializer
+    http_method_names = ['get', 'post']
 
