08616aa9f8cb0f7bc9711dd714c11892d76203ee
Hetag <khetag.urtayev@mail.ru>
2026-05-10 00:13:32 +0300

    list(): фильтрация по email

--- passes_api/views.py
+++ passes_api/views.py
@@ -6,7 +6,7 @@ from .models import Pereval
 
 
 class PerevalApiView(viewsets.ModelViewSet):
-    queryset =Pereval.objects.all()
+    queryset = Pereval.objects.all()
     serializer_class = PerevalSerializer
     http_method_names = ['get', 'post', 'patch']
 
@@ -14,6 +14,7 @@ class PerevalApiView(viewsets.ModelViewSet):
     Переопределяем метод create() для изменения ответов json
     при создании, или попытке создания объекта
     '''
+
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
 
@@ -49,14 +50,14 @@ class PerevalApiView(viewsets.ModelViewSet):
             )
 
     '''
-        Переопределяем метод partial_update() для добавления кастомной логике 
-        при изменении существующего объекта. 
-        1. Запрет на изменение объекта если он не в статусе 'new' 
-        (также данный запрет присутствует в serializers.py)
-        2. Редактировать можно все поля, кроме тех, что 
-        содержат в себе ФИО, адрес почты и номер телефона.
-        3. Запрещено удалять users в целом. 
-        '''
+    Переопределяем метод partial_update() для добавления кастомной логике 
+    при изменении существующего объекта. 
+    1. Запрет на изменение объекта если он не в статусе 'new' 
+    (также данный запрет присутствует в serializers.py)
+    2. Редактировать можно все поля, кроме тех, что 
+    содержат в себе ФИО, адрес почты и номер телефона.
+    3. Запрещено удалять users в целом. 
+    '''
 
     def partial_update(self, request, *args, **kwargs):
         instance = self.get_object()
@@ -111,3 +112,27 @@ class PerevalApiView(viewsets.ModelViewSet):
                 'state': 0,
                 'message': str(e),
             })
+
+    '''
+    Переопределяем list() для получения всех объектов, 
+    либо для получения объектов которые пользовались 
+    определенной почтой. В зависимости от тела запроса
+    '''
+
+    def list(self, request, *args, **kwargs):
+        email = request.query_params.get('user__email')
+        queryset = self.filter_queryset(self.get_queryset())
+
+        if email:
+            queryset = queryset.filter(user__email=email)
+            message = f'Найдено {queryset.count()} объектов для email: {email}'
+        else:
+            message = 'Возвращены все объекты'
+
+        serializer = self.get_serializer(queryset, many=True)
+
+        return Response({
+            'state': 1,
+            'message': message,
+            'data': serializer.data
+        }, status=status.HTTP_200_OK)
