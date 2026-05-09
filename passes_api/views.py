1f7b6c76e94f81332187ea84ccb1f982d1c8dbb1
Hetag <khetag.urtayev@mail.ru>
2026-05-10 00:11:14 +0300

    partial_update: проверка статуса(только new) и защита user‑полей

--- passes_api/views.py
+++ passes_api/views.py
@@ -1,3 +1,4 @@
+from django.forms import model_to_dict
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from .serializers import PerevalSerializer
@@ -7,15 +8,19 @@ from .models import Pereval
 class PerevalApiView(viewsets.ModelViewSet):
     queryset =Pereval.objects.all()
     serializer_class = PerevalSerializer
-    http_method_names = ['get', 'post']
+    http_method_names = ['get', 'post', 'patch']
 
+    '''
+    Переопределяем метод create() для изменения ответов json
+    при создании, или попытке создания объекта
+    '''
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
 
         if not serializer.is_valid():
             return Response(
                 {
-                    'status': 400,
+                    'state': 400,
                     'message': 'Bad Request (при нехватке полей)',
                     'id': None
                 },
@@ -27,7 +32,7 @@ class PerevalApiView(viewsets.ModelViewSet):
 
             return Response(
                 {
-                    'status': 200,
+                    'state': 200,
                     'message': None,
                     'id': pereval.id
                 },
@@ -36,9 +41,73 @@ class PerevalApiView(viewsets.ModelViewSet):
         except Exception as e:
             return Response(
                 {
-                    'status': 500,
+                    'state': 500,
                     'message': str(e),
                     'id': None
                 },
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
+
+    '''
+        Переопределяем метод partial_update() для добавления кастомной логике 
+        при изменении существующего объекта. 
+        1. Запрет на изменение объекта если он не в статусе 'new' 
+        (также данный запрет присутствует в serializers.py)
+        2. Редактировать можно все поля, кроме тех, что 
+        содержат в себе ФИО, адрес почты и номер телефона.
+        3. Запрещено удалять users в целом. 
+        '''
+
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        pereval_data = request.data
+
+        # instance - имеющиеся данные в БД
+        # request - новые данные, обновляют БД
+
+        # Проверяем статус объекта
+        if instance.status != 'new':
+            return Response({
+                'state': 0,
+                'message': f'''Редактирование запрещено: объект имеет статус {instance.status}''',
+            })
+
+        # Проверяем поля, которые нельзя менять
+        user_data = pereval_data.get('user')
+        user_dict = model_to_dict(instance.user)
+        user_dict.pop('id')
+        if user_data and user_dict != user_data:
+            return Response({
+                'state': 0,
+                'message': 'Нельзя редактировать пользователя',
+            })
+
+        # устанавливаем запрет на редактирование статуса
+        pereval_status = request.data.get('status')
+        if pereval_status and pereval_status != instance.status:
+            return Response({
+                'state': 0,
+                'message': 'Нельзя редактировать статус',
+            })
+
+        kwargs['partial'] = True
+        serializer = self.get_serializer(instance, data=pereval_data, partial=True)
+
+        if not serializer.is_valid():
+            return Response({
+                'state': 0,
+                'message': 'Ошибка валидации данных',
+                'errors': serializer.errors,
+            })
+
+        try:
+            self.perform_update(serializer)  # сохранение данных
+            return Response({
+                'state': 1,
+                'message': 'Данные успешно обновлены',
+            })
+        except Exception as e:
+            return Response({
+                'state': 0,
+                'message': str(e),
+            })
