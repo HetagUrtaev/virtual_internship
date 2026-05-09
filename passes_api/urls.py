14b2e650f1ee62b6cfccde4ebf6294c6a7f3ca8b
Hetag <khetag.urtayev@mail.ru>
2026-05-10 00:28:22 +0300

    docs: добавить URL для Swagger‑документации

--- passes_api/urls.py
+++ passes_api/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path, include
 from rest_framework import routers
 from .views import PerevalApiView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
 
 router = routers.DefaultRouter()
 router.register(r'submitData', PerevalApiView, basename='pereval')
 
+
+
 urlpatterns = [
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('', include(router.urls)),
 ]
